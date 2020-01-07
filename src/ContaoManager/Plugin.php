16238226e122ff23f07dcccbe779766cfe7bd90a
Florian Ratz <florian.ratz@supsign.ch>
2020-01-07 16:07:33 +0100

    almost ...

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Supsign\ContaoHelloWordBundle\ContaoManager;
+namespace Supsign\ContaoAttendanceListBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Supsign\ContaoHelloWordBundle\ContaoHelloWordBundle;
+use Supsign\ContaoAttendanceListBundle\ContaoAttendanceListBundle;
 
 class Plugin implements BundlePluginInterface
 {
@@ -16,7 +16,7 @@ class Plugin implements BundlePluginInterface
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(ContaoHelloWordBundle::class)
+            BundleConfig::create(ContaoAttendanceListBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
