2eab4ff18596578c8399bc2f445187efa076029a
Florian Ratz <florian.ratz@supsign.ch>
2020-01-15 08:50:36 +0100

    template rendering now works as intended

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -2,15 +2,15 @@
 
 namespace Supsign\ContaoAttendanceListBundle\Controller;
 
-use Contao\BackendAlerts;
-use Contao\BackendConfirm;
-use Contao\BackendFile;
-use Contao\BackendHelp;
-use Contao\BackendIndex;
-use Contao\BackendMain;
-use Contao\BackendPage;
-use Contao\BackendPassword;
-use Contao\BackendPopup;
+// use Contao\BackendAlerts;
+// use Contao\BackendConfirm;
+// use Contao\BackendFile;
+// use Contao\BackendHelp;
+// use Contao\BackendIndex;
+// use Contao\BackendMain;
+// use Contao\BackendPage;
+// use Contao\BackendPassword;
+// use Contao\BackendPopup;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,15 +30,8 @@ class BackendController extends AbstractController
 
     public function backendRouteAction()
     {
-
-        $var = [
-            'foo' => 'bar'
-        ];
-
-
-        $controller = new BackendMain();
-
-        return $controller->run();
-        // return new Response($this->get('twig')->render('@ContaoAttendanceListBundle/templates/my_backend_route.html.twig', $var)); // Achtung Pfad muss im root/templates von Contao nicht dem Vendor liegen. Hier fehlt die Anpassung und Namensregistrierung mit @SupsignAttendance....
+        return new Response(
+        	$this->get('twig')->render('@ContaoAttendanceList/my_backend_route.html.twig', [])
+        );    
     }
 }
