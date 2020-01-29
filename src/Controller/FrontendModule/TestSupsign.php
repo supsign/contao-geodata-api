53782522a3ea31bd784f700dd5bb4aa88490a0a0
Florian Ratz <florian.ratz@supsign.ch>
2020-01-29 13:34:30 +0100

    dummy controller

---
+++ src/Controller/FrontendModule/TestSupsign.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Supsign\ContaoGeoDataApiBundle\Controller\FrontendModule;
+
+use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\ServiceAnnotation\FrontendModule;
+use Contao\ModuleModel;
+use Contao\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ *
+ * @FrontendModule(category="Bottom")
+ */
+
+
+class TestSupsign extends AbstractFrontendModuleController
+{
+
+	public function test() {
+
+		var_dump(
+			'we have contact!'
+		);
+	}
+
+    protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
+    {
+        return $template->getResponse();
+    }
+}
