08e857d225f6b2aefd3ad110f239deed22282b86
Florian Ratz <florian.ratz@supsign.ch>
2020-01-29 11:33:10 +0100

    more cleanup

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -7,11 +7,9 @@ use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Supsign\ContaoGeoDataApiBundle\ContaoGeoDataApiBundle;
 
-class Plugin implements BundlePluginInterface, RoutingPluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
