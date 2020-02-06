230e5b500440bc4fbefaaa73010bf730d879fcac
Florian Ratz <florian.ratz@supsign.ch>
2020-02-06 15:52:05 +0100

    move to folder

--- src/Resources/public/api/query.php
+++ src/Resources/public/api/query.php
@@ -2,8 +2,6 @@
 
 use Supsign\ContaoGeoDataApiBundle\Controller\FrontendModule\GeoDataApi;
 
-var_dump(__NAMESPACE__);
-
 if (isset($_GET['search']) ) {
 
 	echo (new GeoDataApi)->getCityOrZipJson($_GET['search']);
