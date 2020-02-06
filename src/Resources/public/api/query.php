a2dc6be1fb2e3df8bbbf394fc3a69415bcebecc8
Florian Ratz <florian.ratz@supsign.ch>
2020-02-06 16:11:58 +0100

    some formatting RAUN-17

--- src/Resources/public/api/query.php
+++ src/Resources/public/api/query.php
@@ -1,10 +1,7 @@
 <?php
-
 require dirname(dirname(dirname(__DIR__) ) ).'/Controller/FrontendModule/GeoDataApi.php';
 
 use Supsign\ContaoGeoDataApiBundle\Controller\FrontendModule\GeoDataApi;
 
-if (isset($_GET['search']) ) {
-
+if (isset($_GET['search']) )
 	echo (new GeoDataApi)->getCityOrZipJson($_GET['search']);
-}
